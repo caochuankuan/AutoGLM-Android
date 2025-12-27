54a27ab1ea674b2f211b31dceae4ed41794d88bf
chuankuan cao <917127374@qq.com>
2025-12-27 20:07:36 +0800

    feat: 更新版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.yifeng.autogml"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
